db49c1cf151d776e5285dd58af9543ac57e1aaba
sandeep325 <negisandeep9819@gmail.com>
2024-04-14 16:08:45 +0530

    cloudinary set up

--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -1,10 +1,11 @@
 import { v2 as cloudinary } from "cloudinary";
 import fs from "fs";
 cloudinary.config({
-  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-  api_key: process.env.CLOUDINARY_API_KEY,
-  api_secret: process.env.CLOUDINARY_API_SECRET,
+  cloud_name: CLOUDINARY_CLOUD_NAME,
+  api_key: CLOUDINARY_API_KEY,
+  api_secret: CLOUDINARY_API_SECRET
 });
+
 
 const UploadOnCloudinary = async (localpath) => {
   try {
@@ -16,9 +17,9 @@ const UploadOnCloudinary = async (localpath) => {
     console.log("File uploaded on cloudinary. Response : ", response);
     return response;
   } catch (error) {
-   fs.unlinkSync(localpath); //remove the locally saved file as the upload opration got failed.
-   return null;
+    fs.unlinkSync(localpath); //remove the locally saved file as the upload opration got failed.
+    return null;
   }
 };
 
-export {UploadOnCloudinary}
+export { UploadOnCloudinary }
