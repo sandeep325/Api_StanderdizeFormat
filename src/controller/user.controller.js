3ac429d4a8122651b1301cd63db40c6501a3e502
sandeep325 <negisandeep9819@gmail.com>
2024-04-14 21:35:26 +0530

    commit for controller defination

--- src/controller/user.controller.js
+++ src/controller/user.controller.js
@@ -1,10 +1,22 @@
 import { asyncHandler } from "../utils/asyncHandler.js";
 
 const RegisterUser = asyncHandler( async(req,res)=>{
- res.status(200).json({
-    message:"OK",
+//  get user detail from front end
+const {username,fullName,email,password} = req?.body;
+console.log("email:",email);
+// validation for user detail
+// check if user already exist:username,email
+// check for file : images,check for avatar
+// upload file to cloudinary,get url
+// create user object
+// save user detail to database
+// remove password and refresh token field from response
+// check for user creation 
+// send response to front end
 
- });
+
+
+
 });
 
 export {RegisterUser}
